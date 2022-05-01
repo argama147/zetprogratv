df4e33f95eac192f3d4f23f1f389c2b263c7e71a
argama147 <argama147@yahoo.co.jp>
2022-05-01 20:57:53 +0900

    勤怠表名修正

--- Qt/EARC/examples/OdbcExample/main.cpp
+++ Qt/EARC/examples/OdbcExample/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
     ExcelAccessorODBC accessor;
     QList<QList<QString>> list;
-    if (accessor.read("C:/Users/argam/Desktop/勤怠表2.xls", "シート", list)) {
+    if (accessor.read("C:/Users/argam/Desktop/勤怠表.xls", "シート", list)) {
         qInfo() << list;
     } else {
         qWarning() << "read failed.";
